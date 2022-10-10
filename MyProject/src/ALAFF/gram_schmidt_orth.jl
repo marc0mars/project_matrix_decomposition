057286e54cf2c127450b799a1fe453532c8ea7f2
Marco <htcmarco99@gmail.com>
2022-10-10 08:31:44 +0200

    added Gram_schmidt algorithms and tests belonging to those

--- MyProject/src/ALAFF/gram_schmidt_orth.jl
+++ MyProject/src/ALAFF/gram_schmidt_orth.jl
@@ -1,13 +1,25 @@
 using Test
 using LinearAlgebra
 
+function adj(x)
+    return transpose(conj(x))
+end
+
+"""
+    gram_schmidt(A)
+
+    A: Matrix
+
+    This utilizes the gram schmidt algorithm in order to compute the qr factorization! 
+    This method is not as good as those following bellow.
+"""
 function gram_schmidt(A::Matrix)
     m, n = size(A)
-    rank(A) == n || throw(SingularException)
+    rank(A) == n || throw(error("The matrix is singular!"))
     Q = fill(0.0, m, n)
     R = fill(0.0, n, n)
     for k in 1:n
-        R[1:(k-1), k] = conj(transpose(Q[:,1:k-1]))*A[:,k] 
+        R[1:(k-1), k] = adj(Q[:,1:k-1])*A[:,k] 
         q = A[:,k] - (Q*R[:, k])
         R[k,k] = norm(q)
         Q[:,k] = q/R[k,k]
@@ -15,4 +27,28 @@ function gram_schmidt(A::Matrix)
     return(Q, R)
 end
 
+"""
+    gram_schmidt2(A)
 
+    A: Matrix
+
+    This algorithm utilizes the fact that we dont need the j-th column of A after we have already computed the j-th column of Q.
+    Therefore Q can be saved inside of A.
+
+    This is also known of modified gram-schmidt (MGS)
+"""
+function gram_schmidt2(A::Matrix)
+    M = copy(A) # necessary because this time we save the unitary elements into A but dont want to change matrix A 
+    n = size(A, 2)
+    rank(M) == n || throw(error("The matrix is singular!"))
+    R = fill(0.0, n, n)
+    for j in 1:n
+        for k in 1:j-1
+            R[k,j] = adj(M[:,k])*M[:,j]
+            M[:,j] = M[:,j] - R[k,j]*M[:,k]
+        end
+        R[j,j] = (norm(A[:,j]))
+        M[:,j] = M[:,j]/R[j,j]
+    end
+    return(M, R)
+end
--- MyProject/src/ALAFF/test.jl
+++ MyProject/src/ALAFF/test.jl
@@ -3,17 +3,27 @@ include("./gram_schmidt_orth.jl")
 
 @testset "Q is unitary" begin
     one_mat = [(i == j) ? 1 : 0 for i in 1:7, j in 1:7]
-    for i in 1:500
+    for i in 1:5
         A = rand(10,7)
-        Q = gram_schmidt(A)[1]
+        Q = gram_schmidt2(A)[1]
         @test isapprox(transpose(Q)*Q, one_mat)
     end
 end
 
 @testset "function detects when A does not have full rank" begin
-
+    mat = [1 2 3; 1 2 5; 2 4 19]
+    try 
+        gram_schmidt2(mat)
+    catch err
+        @test isa(err, Exception)
+        @test err.msg == "The matrix is singular!"
+    end
 end
 
 @testset "function computes correct QR decomposition" begin
-
+    for i in 1:5
+        A = rand(10,7)
+        Q, R = gram_schmidt2(A)   
+        @test isapprox(Q*R,A)
+    end
 end
